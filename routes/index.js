107c104081dfc67889bd2398d643bd23c89674a9
Joaquin Mizerit <joaquinmizerit@gmail.com>
2024-08-23 14:48:50 -0300

    Version final proyecto

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
-
 var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.splice(0, 5);
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/img/noimage.png'
+      }
+    }
+  });
+
+  res.render('index', {
+    novedades
+  });
 });
 
 router.post('/', async(req, res, next) => {
